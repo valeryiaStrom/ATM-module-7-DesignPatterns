ade411abc76a1d5cc3ad8fdea1ecebf93cc7d5b6
Valeryia Strom <vstrom@its.jnj.com>
2020-04-26 21:36:58 +0300

    update path

--- test/utils/page_objects/search_results_page/search_results_page.js
+++ test/utils/page_objects/search_results_page/search_results_page.js
@@ -1,5 +1,5 @@
 const AbstractPage = require('../abstract_page/abstract_page');
-const Collection = require('../base-elements/base_collection');
+const Collection = require('../base_elements/base_collection');
 
 class SearchResultsPage extends AbstractPage {
   constructor() {
